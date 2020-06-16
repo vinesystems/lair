1703bf11eff7c9061154993c3504af3a0c65ba12
Min Kim <msk@dolbo.net>
2020-06-15 18:28:51 -0700

    Add benchmarks for all the low-level routines

--- benches/getrf.rs
+++ benches/getrf.rs
@@ -1,21 +1,22 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use lair::decomposition::QRFactorized;
+use lair::decomposition::LUFactorized;
 use ndarray::Array;
 use ndarray_rand::rand::SeedableRng;
 use ndarray_rand::rand_distr::Uniform;
 use ndarray_rand::RandomExt;
 use rand_isaac::isaac64::Isaac64Rng;
+use std::convert::TryFrom;
 
-fn geqrf_100(c: &mut Criterion) {
+fn getrf_100(c: &mut Criterion) {
     let mut rng = Isaac64Rng::seed_from_u64(0);
     let a = Array::random_using((100, 100), Uniform::new(0., 10.), &mut rng);
-    c.bench_function("geqrf (Gram-Schmidt)", |bencher| {
+    c.bench_function("getrf", |bencher| {
         bencher.iter(|| {
             let a = a.clone();
-            QRFactorized::from(a);
+            LUFactorized::try_from(a).expect("non-singular");
         })
     });
 }
 
-criterion_group!(benches, geqrf_100);
+criterion_group!(benches, getrf_100);
 criterion_main!(benches);
--- benches/getrs.rs
+++ benches/getrs.rs
@@ -1,21 +1,23 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use lair::decomposition::QRFactorized;
+use lair::decomposition::LUFactorized;
 use ndarray::Array;
 use ndarray_rand::rand::SeedableRng;
 use ndarray_rand::rand_distr::Uniform;
 use ndarray_rand::RandomExt;
 use rand_isaac::isaac64::Isaac64Rng;
+use std::convert::TryFrom;
 
-fn geqrf_100(c: &mut Criterion) {
+fn getrs_100(c: &mut Criterion) {
     let mut rng = Isaac64Rng::seed_from_u64(0);
     let a = Array::random_using((100, 100), Uniform::new(0., 10.), &mut rng);
-    c.bench_function("geqrf (Gram-Schmidt)", |bencher| {
+    let b = Array::random_using(100, Uniform::new(0., 10.), &mut rng);
+    let lu = LUFactorized::try_from(a).expect("non-singular");
+    c.bench_function("getrs", |bencher| {
         bencher.iter(|| {
-            let a = a.clone();
-            QRFactorized::from(a);
+            lu.solve(&b);
         })
     });
 }
 
-criterion_group!(benches, geqrf_100);
+criterion_group!(benches, getrs_100);
 criterion_main!(benches);
