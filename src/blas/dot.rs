dc4c28a15e3b908cd27798fe8a8ee388b5e29cde
Min Kim <msk@dolbo.net>
2020-06-30 06:12:29 -0700

    Add safety conditions to dot

--- src/blas/dot.rs
+++ src/blas/dot.rs
@@ -1,12 +1,26 @@
 use std::iter::Sum;
 use std::ops::Mul;
 
-#[allow(clippy::cast_possible_wrap, clippy::similar_names)]
-pub unsafe fn dot<T>(n: usize, x: *const T, incx: isize, y: *const T, incy: isize) -> T
+/// Computes a dot product of two vectors.
+///
+/// # Safety
+///
+/// * `x` is the beginning address of an array of at least `n` elements with
+///   stride `inc_x`.
+/// * `y` is the beginning address of an array of at least `n` elements with
+///   stride `inc_y`.
+/// * `(n - 1) * inc_x` and `(n - 1) * inc_y` are between `isize::MIN` and
+///   `isize::MAX`, inclusive.
+#[allow(clippy::cast_possible_wrap)]
+pub unsafe fn dot<T>(n: usize, x: *const T, inc_x: isize, y: *const T, inc_y: isize) -> T
 where
     T: Copy + Mul + Sum<<T as Mul>::Output>,
 {
+    debug_assert!(n <= isize::MAX as usize);
+    debug_assert!((n as isize - 1).checked_mul(inc_x).is_some());
+    debug_assert!((n as isize - 1).checked_mul(inc_y).is_some());
+
     (0..n)
-        .map(|i| *x.offset(incx * i as isize) * *y.offset(incy * i as isize))
+        .map(|i| *x.offset(inc_x * i as isize) * *y.offset(inc_y * i as isize))
         .sum()
 }
