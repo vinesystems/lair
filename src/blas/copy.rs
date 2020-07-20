43ceb80e896eee04ced2d719cd229476c1fb8f97
Min Kim <msk@dolbo.net>
2020-07-20 07:40:42 -0700

    Complete copy

--- src/blas/copy.rs
+++ src/blas/copy.rs
@@ -9,11 +9,11 @@
 /// * The `n` elements of `x` must have been initialized.
 #[allow(dead_code)]
 pub(crate) unsafe fn copy<T: Copy>(n: usize, x: *const T, inc_x: isize, y: *mut T, inc_y: isize) {
-    let x_ptr = x;
-    let y_ptr = y;
+    let mut x_ptr = x;
+    let mut y_ptr = y;
     for _ in 0..n {
         *y_ptr = *x_ptr;
-        x_ptr.offset(inc_x);
-        y_ptr.offset(inc_y);
+        x_ptr = x_ptr.offset(inc_x);
+        y_ptr = y_ptr.offset(inc_y);
     }
 }
