b150ecf72150f15f2f9b24734d65d329b7ba61d1
Min Kim <msk@dolbo.net>
2021-03-12 08:28:11 -0800

    Make `lascl::full` safe

--- src/lapack/lascl.rs
+++ src/lapack/lascl.rs
@@ -1,21 +1,15 @@
 use crate::{Float, One, Real, Scalar, Zero};
+use ndarray::{ArrayBase, DataMut, Ix2};
 
 /// Multiplies a full matrix by a real scalar `c_to / c_from`.
-#[allow(clippy::cast_possible_wrap)]
 #[allow(dead_code)]
-pub(crate) unsafe fn full<T>(
-    c_from: T::Real,
-    c_to: T::Real,
-    m: usize,
-    n: usize,
-    a: *mut T,
-    row_stride: isize,
-    col_stride: isize,
-) where
-    T: Scalar,
+pub(crate) fn full<A, S>(c_from: A::Real, c_to: A::Real, a: &mut ArrayBase<S, Ix2>)
+where
+    A: Scalar,
+    S: DataMut<Elem = A>,
 {
-    let small_num = T::Real::sfmin();
-    let big_num = T::Real::one() / small_num;
+    let small_num = A::Real::sfmin();
+    let large_num = A::Real::one() / small_num;
 
     let mut c_from = c_from;
     let mut c_to = c_to;
@@ -26,30 +20,40 @@ pub(crate) unsafe fn full<T>(
             if small_from == c_from {
                 ((c_to / c_from).into(), true)
             } else {
-                let small_to = c_to / big_num;
+                let small_to = c_to / large_num;
                 if small_to == c_to {
-                    c_from = T::Real::one();
+                    c_from = A::Real::one();
                     ((c_to).into(), true)
-                } else if small_from.abs() > c_to.abs() && c_to.abs() != T::Real::zero() {
+                } else if small_from.abs() > c_to.abs() && c_to.abs() != A::Real::zero() {
                     c_from = small_from;
                     ((small_num).into(), false)
                 } else if small_to.abs() > c_from {
                     c_to = small_to;
-                    ((big_num).into(), false)
+                    ((large_num).into(), false)
                 } else {
                     ((c_to / c_from).into(), true)
                 }
             }
         };
 
-        for i in 0..m {
-            for j in 0..n {
-                *a.offset(i as isize * row_stride + j as isize * col_stride) *= mul;
-            }
+        for v in a.iter_mut() {
+            *v *= mul;
         }
 
         if done {
             break;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ndarray::arr2;
+
+    #[test]
+    fn full() {
+        let mut a = arr2(&[[3., 2.], [4., 3.]]);
+        super::full(2., 4., &mut a);
+        assert_eq!(a, arr2(&[[6., 4.], [8., 6.]]));
     }
 }
