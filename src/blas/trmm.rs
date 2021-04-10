c757504ea6f26ce18a62b1bf93944597cf881707
Min Kim <msk@dolbo.net>
2021-04-09 19:24:33 -0700

    Add trmm for conj trans of upper trianglular matrix to the right

--- src/blas/trmm.rs
+++ src/blas/trmm.rs
@@ -39,6 +39,46 @@ pub fn right_lower_notrans<A, SA, SB, const UNIT: bool>(
     }
 }
 
+/// Computes `b` * `a^H` assuming `a` is a upper triangular matrix.
+///
+/// `UNIT` indicates whether to assume `a`'s diagonal elements are 1s.
+///
+/// # Panics
+///
+/// Panics if `a` is not a square matrix, or `a`'s # of rows is different from
+/// `b`'s # of columns.
+#[allow(dead_code)]
+pub fn right_upper_conjtrans<A, SA, SB, const UNIT: bool>(
+    a: &ArrayBase<SA, Ix2>,
+    b: &mut ArrayBase<SB, Ix2>,
+) where
+    A: Scalar,
+    SA: Data<Elem = A>,
+    SB: DataMut<Elem = A>,
+{
+    assert!(a.is_square());
+    assert_eq!(b.ncols(), a.nrows());
+    for k in 0..a.ncols() {
+        for j in 0..k {
+            let multiplier = a[(j, k)].conj();
+            if multiplier == A::zero() {
+                continue;
+            }
+
+            for i in 0..b.nrows() {
+                let b_i_k = b[(i, k)];
+                b[(i, j)] += multiplier * b_i_k;
+            }
+        }
+        if !UNIT {
+            let multiplier = a[(k, k)].conj();
+            if multiplier != A::one() {
+                b.column_mut(k).mul_assign(multiplier);
+            }
+        }
+    }
+}
+
 /// Computes `b` * `a^H` assuming `a` is a lower triangular matrix.
 ///
 /// `UNIT` indicates whether to assume `a`'s diagonal elements are 1s.
@@ -90,6 +130,26 @@ mod tests {
         let mut b = arr2(&[[-2., 3.], [-3., 1.]]);
         super::right_lower_notrans::<_, _, _, false>(&a, &mut b);
         assert_eq!(b, arr2(&[[5., -9.], [4., -3.]]));
+    }
+
+    #[test]
+    fn right_upper_conjtrans_nounit() {
+        let a = arr2(&[
+            [Complex64::new(-1., 2.), Complex64::new(2., -3.)],
+            [Complex64::new(2., 1.), Complex64::new(-3., 1.)],
+        ]);
+        let mut b = arr2(&[
+            [Complex64::new(-2., 1.), Complex64::new(3., 1.)],
+            [Complex64::new(-3., -1.), Complex64::new(1., 2.)],
+        ]);
+        super::right_upper_conjtrans::<_, _, _, false>(&a, &mut b);
+        assert_eq!(
+            b,
+            arr2(&[
+                [Complex64::new(7., 14.), Complex64::new(-8., -6.)],
+                [Complex64::new(-3., 14.), Complex64::new(-1., -7.)]
+            ])
+        );
     }
 
     #[test]
